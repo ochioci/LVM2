da654360cec93833b52ab08eb253b585cd8fc5b6
Beckett <beckettrandlettapps@gmail.com>
2022-03-28 01:17:12 -0400

    init

--- LogicalVolume.java
+++ LogicalVolume.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class LogicalVolume extends Volume{
-    public LogicalVolume (String name, int size) {
+    private VolumeGroup assignedVG;
+    public LogicalVolume (String name, int size, VolumeGroup vg) {
         super(name, size);
+        this.assignedVG = vg;
     }
 }
--- VolumeGroup.java
+++ VolumeGroup.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.UUID;
 public class VolumeGroup extends Volume{
     private ArrayList<PhysicalVolume> volumes;
+    private ArrayList<LogicalVolume> LVMS = new ArrayList<LogicalVolume>();
     public VolumeGroup (String name, ArrayList<PhysicalVolume> volumes) {
         super(name, getTotalSize(volumes));
         this.volumes = volumes;
@@ -13,4 +14,11 @@ public class VolumeGroup extends Volume{
         }
         return total;
     }
+
+
+    public void addLVM (LogicalVolume LV) {
+        this.LVMS.add(LV);
+    }
+
+
 }
